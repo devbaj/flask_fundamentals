fa0974a4d52ae090ad94bfc1b53888e0426a028e
Brett Jory <bjory7@gmail.com>
2019-04-02 16:47:40 -0700

    made changes to follow-along exercise

--- form_test/server.py
+++ form_test/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = "to blaive"
 
 @app.route("/")
 def index():
@@ -9,9 +10,18 @@ def index():
 def create_user():
     print("Got Post Info")
     print(request.form)
-    name_from_form = request.form["name"]
-    email_from_form = request.form["email"]
-    return render_template("show.html", name_on_template=name_from_form, email_on_template=email_from_form)
+    session["form"] = request.form
+    session["username"] = request.form["name"]
+    session["useremail"] = request.form["email"]
+    return redirect("/show")
+
+@app.route("/show")
+def show_user():
+    print("*"*30)
+    print("Showing the User Info from the Form")
+    print(session["form"])
+    print("*"*30)
+    return render_template("show.html", name_on_template=session["username"], email_on_template=session["useremail"])
 
 
 if __name__ == "__main__":
