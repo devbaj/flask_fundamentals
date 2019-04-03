db9c9b0a93caefee2621e381f4ce02102fb2fda2
Brett Jory <bjory7@gmail.com>
2019-04-03 11:07:24 -0700

    finished win condition feature

--- ninja_gold/server.py
+++ ninja_gold/server.py
@@ -8,17 +8,23 @@ now = datetime.datetime.now()
 
 @app.route("/")
 def index():
-    if "counter" in session:
+    session["wincondition"] = 100
+    if "counter" in session and session["counter"] < 10:
         session["counter"] += 1
+    elif "counter" in session and session["counter"] >= 10:
+        if session["gold"] >= int(session["wincondition"]):
+            session["win"] = True
+        else:
+            session["win"] = False
     else:
         session["counter"] = 1
         session["gold"] = 0
         session["ledger"] = ""
-        session["ledgerlist"] = []
     return render_template("index.html")
 
 @app.route("/process_money", methods=["POST"])
 def process():
+    now = datetime.datetime.now()
     session["ledger"] += "<p class='"
     if request.form["place"] == "farm":
         change = random.randint(10, 20)
@@ -39,11 +45,8 @@ def process():
             session["ledger"] += "gain'>Entered the casino and won " + str(change) + " gold!"
         elif change < 0:
             session["ledger"] += "loss'>Entered the casino and lost " + str(abs(change)) + " gold!"
-    session["ledger"] += " (" + str(now) + ")</p>|"
+    session["ledger"] += " (" + str(now.year) + "/" + str(now.month) + "/" + str(now.day) + " at " + str(now.hour) + ":" + str(now.minute) + ")</p>|"
     session["ledgerList"] = session["ledger"].split("|")
-    print("*"*30)
-    print(session["ledgerList"])
-    print("*"*30)
     return redirect("/")
 
 @app.route("/reset", methods=["POST"])
