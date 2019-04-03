a536f11ca87bd324cf745dfbd18321d26d3e16c9
Brett Jory <bjory7@gmail.com>
2019-04-03 13:49:40 -0700

    simplified server-side logic, using a dictionary to cut down on conditionals. Also transferred some of the string-buliding to index.html from server.py, and added logic necessary to complete that functionality in server.py

--- ninja_gold/server.py
+++ ninja_gold/server.py
@@ -8,11 +8,20 @@ now = datetime.datetime.now()
 
 @app.route("/")
 def index():
-    session["wincondition"] = 100
-    if "counter" in session and session["counter"] < 10:
+    session["wincondition"] = {
+        "turns": 10,
+        "gold": 100
+    }
+    session["placeDict"] = {
+        "farm": [10,20],
+        "cave": [5, 10],
+        "house": [2, 5],
+        "casino": [-50, 50]
+    }
+    if "counter" in session and session["counter"] < session["wincondition"]["turns"]:
         session["counter"] += 1
-    elif "counter" in session and session["counter"] >= 10:
-        if session["gold"] >= int(session["wincondition"]):
+    elif "counter" in session and session["counter"] >= session["wincondition"]["turns"]:
+        if session["gold"] >= int(session["wincondition"]["gold"]):
             session["win"] = True
         else:
             session["win"] = False
@@ -20,33 +29,34 @@ def index():
         session["counter"] = 1
         session["gold"] = 0
         session["ledger"] = ""
+        session["gain"] = ""
     return render_template("index.html")
 
 @app.route("/process_money", methods=["POST"])
 def process():
     now = datetime.datetime.now()
-    session["ledger"] += "<p class='"
-    if request.form["place"] == "farm":
-        change = random.randint(10, 20)
-        session["gold"] += change
-        session["ledger"] += "gain'>Entered the farm and earned " + str(change) + " gold!"
-    elif request.form["place"] == "cave":
-        change = random.randint(5, 10)
-        session["gold"] += change
-        session["ledger"] += "gain'>Entered the cave and earned " + str(change) + " gold!"
-    elif request.form["place"] == "house":
-        change = random.randint(5, 10)
-        session["gold"] += change
-        session["ledger"] += "gain'>Entered the house and earned " + str(change) + " gold!"
-    elif request.form["place"] == "casino":
-        change = random.randint(-50, 50)
-        session["gold"] += change
-        if change >= 0:
-            session["ledger"] += "gain'>Entered the casino and won " + str(change) + " gold!"
-        elif change < 0:
-            session["ledger"] += "loss'>Entered the casino and lost " + str(abs(change)) + " gold!"
-    session["ledger"] += " (" + str(now.year) + "/" + str(now.month) + "/" + str(now.day) + " at " + str(now.hour) + ":" + str(now.minute) + ")</p>|"
+    change = random.randint(session["placeDict"][request.form["place"]][0], session["placeDict"][request.form["place"]][1])
+    
+    nowString = "(" + str(now.year) + "/" + str(now.month) + "/" + str(now.day) + " at " + str(now.hour) + ":" + str(now.minute) + ")"
+    
+    session["gold"] += change
+    if change >= 0:
+        session["gain"] += "gain|"
+    else:
+        session["gain"] += "loss|"
+    
+    session["ledger"] += "Entered the " + request.form["place"] + " and "
+    
+    if session["gain"] == "gain" :
+        session["ledger"] += "got"
+    else:
+        session["ledger"] += "lost"
+    
+    session["ledger"] += " " + str(change) + " gold!"
+    session["ledger"] += " " + nowString + "|"
+    
     session["ledgerList"] = session["ledger"].split("|")
+    session["gainList"] = session["gain"].split("|")
     return redirect("/")
 
 @app.route("/reset", methods=["POST"])
